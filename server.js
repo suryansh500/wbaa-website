82ac376bc9e9a0f544dbdd6c8c840d5e0d3899a9
Suryansh Singh <suryansh.62004@gmail.com>
2026-03-14 08:58:36 +0530

    final atlas connnection fix

--- server.js
+++ server.js
@@ -71,10 +71,16 @@ mongoose.connect(process.env.MONGO_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .then(() => { 
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+  console.log("Server running on port" + PORT);
+});
+
+  })
+  .catch(err => { console.log("MongoDB connection error:",err)});
 
 // start server (MUST BE LAST)
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log("Server running on port" + PORT);
 });
