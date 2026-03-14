96347df2761f921fa8f93e999565afc3c8f07f9f
Suryansh Singh <suryansh.62004@gmail.com>
2026-03-14 08:32:18 +0530

    fixed mongo

--- server.js
+++ server.js
@@ -67,7 +67,10 @@ app.get('/', async (req, res) => {
 });
 
 // db
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
